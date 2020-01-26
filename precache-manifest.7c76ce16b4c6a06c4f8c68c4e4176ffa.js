bdd9647b22a116e47586c1dd3c7010ce44b08f55
baramofer <56403621+baramofer@users.noreply.github.com>
2020-01-26 15:24:39 +0200

    Add files via upload

---
+++ precache-manifest.7c76ce16b4c6a06c4f8c68c4e4176ffa.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "727106f98427350eec721a5dee2f0048",
+    "url": "https://baramofer.github.io/bitApp/index.html"
+  },
+  {
+    "revision": "64398662f6bfdeeb5e95",
+    "url": "https://baramofer.github.io/bitApp/static/css/2.400f043a.chunk.css"
+  },
+  {
+    "revision": "4c9b0ff298b34bdec6a4",
+    "url": "https://baramofer.github.io/bitApp/static/css/main.bcf3d8e3.chunk.css"
+  },
+  {
+    "revision": "64398662f6bfdeeb5e95",
+    "url": "https://baramofer.github.io/bitApp/static/js/2.2087196d.chunk.js"
+  },
+  {
+    "revision": "39c9fe7992ba29e7c688ffdd3898ae46",
+    "url": "https://baramofer.github.io/bitApp/static/js/2.2087196d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4c9b0ff298b34bdec6a4",
+    "url": "https://baramofer.github.io/bitApp/static/js/main.f38f2a14.chunk.js"
+  },
+  {
+    "revision": "1712a2f0340aaac95c5b",
+    "url": "https://baramofer.github.io/bitApp/static/js/runtime-main.82ce9bfb.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "https://baramofer.github.io/bitApp/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "https://baramofer.github.io/bitApp/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "https://baramofer.github.io/bitApp/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "https://baramofer.github.io/bitApp/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "https://baramofer.github.io/bitApp/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
